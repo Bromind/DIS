4a129b3bfb0a6d565fe7f676d384c5d18bf76137
Arnaud Wald <arnaudwald@gmail.com>
2015-12-12 15:53:24 +0100

    Excel table + modified variable names

--- matlab/evaluate.m
+++ matlab/evaluate.m
@@ -1,25 +1,38 @@
 clc;
 close all;
 
-mean_perf1 = mean(metric1);
-mean_perf2 = mean(metric2);
+% Concatenate data
+perf1 = [privFixed1_metric1' privFixed2_metric1' privFixed3_metric1' privFixed4_metric1' privFixed5_metric1' privFixed6_metric1'];
+perf2 = [privFixed1_metric2' privFixed2_metric2' privFixed3_metric2' privFixed4_metric2' privFixed5_metric2' privFixed6_metric2'];
+comps = 1:1:length(perf1(1,:));
 
-std_dev1 = std(metric1);
-std_dev2 = std(metric2);
+mean_perf1 = mean(perf1);
+mean_perf2 = mean(perf2);
+std_dev1 = std(perf1);
+std_dev2 = std(perf2);
 
+means1 = [mean_perf1' std_dev1'];
+means2 = [mean_perf2' std_dev2'];
+
+% Plot the results
 figure;
-subplot(1,2,1);
-boxplot( metric1 );
+subplot(2,1,1);
+err1 = std_dev1.*comps;
+hold on
+for i=1:1:length(perf1(1,:))
+    errorbar(comps(i), mean_perf1(i), err1(i), 'x');
+end
+title('First metric');
+ylabel('Events handled per time unit');
+legend('Private, fixed threshold = 5', 'Private, fixed threshold = 8');
 
-subplot(1,2,2);
-boxplot( metric2 );
 
-
-figure
-subplot(1,2,1);
-errorbar(1,mean_perf1,std_dev1,'rx');
-axis([0.95 1.05 0.215 0.23]);
-
-subplot(1,2,2);
-errorbar(1,mean_perf2,std_dev2,'x');
-axis([0.95 1.05 0.5 2]);
+subplot(2,1,2);
+err2 = std_dev2.*comps;
+hold on
+for i=1:1:length(perf2(1,:))
+    errorbar(comps(i), mean_perf2(i), err2(i), 'x');
+end
+title('Second metric');
+ylabel('Distance traveled per event handled');
+legend('Private, fixed threshold = 5', 'Private, fixed threshold = 8');
